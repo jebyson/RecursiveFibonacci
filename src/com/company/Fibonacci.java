469291a09661a7a4a48b258112a9c3759a25b435
je565 <joshuaebyson@yahoo.com>
2018-01-22 08:50:43 -0500

    Initial commit

---
+++ src/com/company/Fibonacci.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by je565 on 1/22/18.
+ */
+public class Fibonacci {
+
+    public static boolean checkFib()
+    {
+
+    }
+
+    public static int fibSequence()
+    {
+
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter a number to check if its a Fibonacci number.");
+
+        Scanner input = new Scanner(System.in);
+        int userNum = input.nextInt();
+    }
+}
