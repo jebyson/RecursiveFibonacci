72127c32cb46b8e7fe6739add903dea3588a6545
jebyson <joshuaebyson@yahoo.com>
2018-01-24 22:21:59 -0500

    Done

--- src/com/company/Fibonacci.java
+++ src/com/company/Fibonacci.java
@@ -1,65 +1,16 @@
 package com.company;
 
-/**
- * Created by je565 on 1/22/18.
- */
 public class Fibonacci {
 
-    private int [] sequence = new int[100000];
-
-    private int fibTotal = 0;
-    private int xTemp = 0;
-    private int yTemp = 1;
-
-    private int numOfSteps = 0;
-
-    public void Fibonacci(int num)
+    public static int fibonacci(int n)
     {
-        for(int j = 0; j < sequence.length; j++)
+        if (n <= 1)
         {
-            fibTotal = xTemp + yTemp;
-            xTemp = yTemp;
-            yTemp = fibTotal;
-
-            sequence[j] = fibTotal;
-
-            if(num == sequence[j])
-            {
-                System.out.println(num + " is a number in the Fibonacci sequnce. Here is the sequence leading up to it:");
-                numOfSteps = j;
-                checkFib(num);
-                break;
-            }
-            else if(j == sequence.length - 1 && num != fibTotal)
-            {
-                System.out.println(num + " is not a number in the Fibonacci sequence.");
-            }
+            return n;
         }
-    }
-
-    public void checkFib(int inputNum)
-    {
-        int fibTotal = 0;
-        int xTemp = 0;
-        int yTemp = 1;
-
-        System.out.print("0, 1, ");
-
-        for(int k = 0; k < numOfSteps; k++)
+        else
         {
-            fibTotal = xTemp + yTemp;
-            xTemp = yTemp;
-            yTemp = fibTotal;
-
-            sequence[k] = fibTotal;
-
-            System.out.print(sequence[k] + ", ");
-
-            if(sequence[k] == inputNum)
-            {
-                System.out.print(" " + inputNum);
-                break;
-            }
+            return fibonacci(n-1) + fibonacci(n-2);
         }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,29 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Enter a number to see if it is a Fibonacci number");
+        Scanner kb = new Scanner(System.in);
+        int num = kb.nextInt();
 
-        System.out.println("Enter a number to check if its a Fibonacci number.");
+        String numbers = "";
+        String numAsString;
+        numAsString = String.valueOf(num);
 
-        Scanner input = new Scanner(System.in);
-        int userNum = input.nextInt();
+        for (int i = -1; i <= num; i++) {
+            int temp = Fibonacci.fibonacci(i + 1);
+            numbers = numbers + temp + ", ";
+            if (temp >= num) {
+                break;
+            }
+        }
 
-        Fibonacci test = new Fibonacci();
-        test.Fibonacci(userNum);
+        numbers = numbers.substring(0, numbers.length() - 2);
+
+        if (numbers.contains(numAsString)) {
+            System.out.println(num + " is a Fibonacci number. Here is the list leading up to it: " + numbers);
+        }
+        else {
+            System.out.println(num + " is not a Fibonacci number.");
+        }
     }
 }
